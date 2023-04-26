f5c4ab7734aec5d0ad19cf9e30f50b5e6b4effc1
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-04-26 14:00:45 -0600

    ok, structure added this time

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose')
+
+// Schema to create User model
+const userSchema = new Schema (
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+\@.+\..+/],
+    },
+    thoughts: [
+        {
+        type: 
+        ref: 'thought'
+        }
+    ],
+    friends: [
+        {
+        type:
+        ref:'friend'
+        }
+    ]
+        },
+        )
